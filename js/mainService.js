4b576f19ee89ad13b12e165fd785fb7da0359fdf
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-06 10:27:54 -0600

    Updated structure changes and a few minor changes

---
+++ js/mainService.js
@@ -0,0 +1,43 @@
+app.service('mainService', function($http) {
+	var notes = [
+		{
+			meta: {
+				title: 'Church Notes 8/30/15',
+				timestamp: '08/30/2015 13:17:00'
+			},
+			
+			announcements: [
+				{
+					title: 'Announcements',
+					announcements: [
+						'Institute kickoff is this Wednesday at 6:00pm which will include food and a talk given by Pres Heaton, followed by games',
+						'FHE this week is at Bro Napp\'s house in Mapleton at 7:00. Carpooling is available at the church at 6:30pm.'
+					]
+				}
+			],
+			
+			speakers: [
+				{
+					
+				}
+			],
+			
+			otherThoughts: {
+				publicNotes: [
+					'Repentence refreshes our view of ourselves, of others, and elevates our view to a higher level and helps us to know how God loves us and helps us to love others like God loves us.'
+				],
+				privateNotes: [
+					
+				]
+			}
+		}
+	];
+	
+	this.getNotes = function() {
+		return notes;
+	};
+	
+	this.postNotes = function(post) {
+		notes.push(post);
+	};
+});
