40e3156e44cd4099ba425cd4d8f29a03eabe94c4
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-11-15 13:08:22 -0700

    Rearranging

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	cors = require('cors'),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
+	PostController = require('./controllers/post'),
 	mongoURI = 'mongodb://localhost:27017/church-notes-portal',
 	port = 8887;
 	
@@ -10,7 +11,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/posts', PostController.getPosts);
 
 
 
